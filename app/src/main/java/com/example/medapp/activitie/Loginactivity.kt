e76a45cea36cf637e7d1adfc0ef0289ae4f6e153
Ashu Choudhary <ashuchoudhary7983@gmail.com>
2025-02-19 01:01:52 +0530

    add login and forget code

--- app/src/main/java/com/example/medapp/activitie/Loginactivity.kt
+++ app/src/main/java/com/example/medapp/activitie/Loginactivity.kt
@@ -1,7 +1,10 @@
 package com.example.medapp.activitie
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.example.medapp.R
 import com.example.medapp.databinding.ActivityLoginBinding
 
 class Loginactivity : AppCompatActivity() {
@@ -14,6 +17,21 @@ class Loginactivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        val forgetPasswordText = findViewById<TextView>(R.id.txt_passforgot)
+
+        // Set Click Listener
+        forgetPasswordText.setOnClickListener {
+            val intent = Intent(this, ForgetPassword::class.java)
+            startActivity(intent)
+        }
+
+        val registerText = findViewById<TextView>(R.id.txt_regster)
+
+        // Set Click Listener
+        registerText.setOnClickListener {
+            val intent = Intent(this, Firstpage::class.java)
+            startActivity(intent)
+        }
 
     }
 
