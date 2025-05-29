e6da1fa071af85aa208f01b919abb05f5cb5de9b
Ashu Choudhary <ashuchoudhary7983@gmail.com>
2025-05-30 00:01:40 +0530

    edit code

--- app/src/main/java/com/example/medapp/activitie/Firstpage.kt
+++ app/src/main/java/com/example/medapp/activitie/Firstpage.kt
@@ -1,9 +1,7 @@
 package com.example.medapp.activitie
 
-import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
@@ -11,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.medapp.R
+import com.example.medapp.doc_activites.Registerdoctor
+import com.example.medapp.pat_activites.Registerpatient
 
 class Firstpage : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,32 +23,23 @@ class Firstpage : AppCompatActivity() {
             insets
         }
 
-        val currentlayout = findViewById<ViewGroup>(R.id.main)
-
         val button1 = findViewById<Button>(R.id.btn_patient)
         val button2 = findViewById<Button>(R.id.btn_doctor)
 
-
-        val newlayout1 = layoutInflater.inflate(R.layout.activity_register_patient, currentlayout, false)
-        val newlayout2 = layoutInflater.inflate(R.layout.activity_register_doctor, currentlayout, false)
-
         button1.setOnClickListener {
-            currentlayout.removeAllViews()
-            currentlayout.addView(newlayout1)
+            val intent = Intent(this, Registerpatient::class.java)
+            startActivity(intent)
         }
 
         button2.setOnClickListener {
-            currentlayout.removeAllViews()
-            currentlayout.addView(newlayout2)
+            val intent = Intent(this, Registerdoctor::class.java)
+            startActivity(intent)
         }
 
         val Login = findViewById<TextView>(R.id.txt_login)
-
-        // Set Click Listener
         Login.setOnClickListener {
             val intent = Intent(this, Loginactivity::class.java)
             startActivity(intent)
         }
-
     }
 }
